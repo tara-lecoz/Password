e0d0246216483f6b7fc9e9a40ca63b146f3b9d84
tara-lecoz <tara.le-coz@laplateforme.io>
2023-02-20 14:31:16 +0100

    modification nom fichier

--- main.py
+++ main.py
@@ -15,21 +15,24 @@ except:
     utilisateur_mdp = {}
 #-----Fonction qui vérifie que le mot de passe respecte les conditions-----
 def verifier_mdp():
+    global mdp
     mdp = input("Votre mot de passe : ")
-    while True:
-        if len(mdp) < 8:
-            print("Le mot de passe n'est pas valide (caractères < 8)")
-        elif not re.search("[A-Z]", mdp):
-            print("/!\ ERREUR /!\ Le mot de passe doit contenir au moins une lettre majuscule.")
-        elif not re.search("[a-z]", mdp):
-            print("/!\ ERREUR /!\ Le mot de passe doit au moins contenir une lettre minuscule.")
-        elif not re.search("[0-9]", mdp):
-            print("/!\ ERREUR /!\ Le mot de passe doit au moins contenir un chiffre.")
-        elif not re.search("[!@#$%^&*]", mdp):
-            print("/!\ ERREUR /!\ Le mot de passe doit contenir au moins un caractère spécial.")
-        else:
-            print("Félicitations, votre mot de passe est valide !")
-            return mdp     
+    if len(mdp) < 8:
+        print("Le mot de passe n'est pas valide (caractères < 8)")
+    elif not re.search("[A-Z]", mdp):
+        print("/!\ ERREUR /!\ Le mot de passe doit contenir au moins une lettre majuscule.")
+    elif not re.search("[a-z]", mdp):
+        print("/!\ ERREUR /!\ Le mot de passe doit au moins contenir une lettre minuscule.")
+    elif not re.search("[0-9]", mdp):
+        print("/!\ ERREUR /!\ Le mot de passe doit au moins contenir un chiffre.")
+    elif not re.search("[!@#$%^&*]", mdp):
+        print("/!\ ERREUR /!\ Le mot de passe doit contenir au moins un caractère spécial.")
+    else:
+        print("Félicitations, votre mot de passe est valide !")
+        return mdp
+        
+if verifier_mdp() == None:
+    verifier_mdp()
 #-----Hachage du mot de passe valide avec l'algorithme SHA-256-----
 def mdp_hashe(mdp):
     mdp_hashe = hashlib.sha256(mdp.encode()).hexdigest()
@@ -37,7 +40,7 @@ def mdp_hashe(mdp):
     return mdp_hashe
 #-----Vérification de la correspondance utilisateur / mot de passe & écriture dans le dictionnaire-----
 utilisateur = input("Veuillez renseigner votre nom d'utilisateur :")
-mdp_json = verifier_mdp()
+mdp_json = mdp
 if utilisateur in utilisateur_mdp:
     if mdp_json in utilisateur_mdp[utilisateur]:
         print("Le mot de passe existe déjà !")
@@ -52,5 +55,5 @@ if mdp_validehash.lower() == 'o':
     print(utilisateur_mdp[utilisateur])
 with open("mdp_utilisateur.json", "w") as verif_utilisateurmdp:
     json.dump(utilisateur_mdp, verif_utilisateurmdp, separators=(",", " : "), indent=4)
-mdp_valider = verifier_mdp()
+mdp_valider = mdp
 mdp_hashe(mdp_valider)
